341940a55e8d9f58cdc7e1988486132f32885683
seolan <seolan25@gmail.com>
2026-03-30 17:39:03 +0900

    Implement Profiler.java

--- src/main/java/org/example/Profiler.java
+++ src/main/java/org/example/Profiler.java
@@ -1,5 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Profiler {
-    // TODO : Profiler 구현
+    ArrayList<Integer> arr;
+    int changeCnt;
+    Sort lib;
+
+    public Profiler(ArrayList<Integer> arr, Sort _sort){
+        this.arr = arr;
+        this.lib = _sort;
+        this.changeCnt = 0;
+    }
+
+    public void setData(ArrayList<Integer> _arr) {
+        this.arr.clear();
+        this.arr.addAll(_arr);
+    }
+
+    public void setLib(Sort _sort){
+        this.lib = _sort;
+    }
+
+    public void runLib() {
+        changeCnt = this.lib.sort(arr);
+    }
+
+    public void showResult() {
+        System.out.println(this.arr);
+        System.out.println(this.changeCnt);
+    }
 }
