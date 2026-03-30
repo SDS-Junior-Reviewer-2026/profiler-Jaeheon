a6206e0bf53cfa8c8a5c9ffdfde45fbc5c123a99
seolan <seolan25@gmail.com>
2026-03-30 17:39:35 +0900

    implement test in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO : Profiler 실행해보기
+        Profiler profiler_1 = new Profiler(
+                new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)),
+                new BubbleSort()
+        );
+
+        profiler_1.runLib();
+        profiler_1.showResult();
+
+        Profiler profiler_2 = new Profiler(
+                new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)),
+                new SelectionSort()
+        );
+
+        profiler_2.runLib();
+        profiler_2.showResult();
     }
 }
